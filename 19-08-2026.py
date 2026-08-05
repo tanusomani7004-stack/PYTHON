6e79b46611403f9ca265a8cc5cc0a9f4f3a8a99d
Tanu <tanusomani7004@gmail.com>
2026-08-05 22:18:06 +0530

    Add student profile script in 19-08-2026.py

---
+++ 19-08-2026.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+a = b
+
+print(a)
+print(b)
+
+name = "Rahul"
+
+Name = "Tanu"
+
+print(name)
+
+print(Name)
+
+
+print("=========== Student Profile===========")
+name = "Tanu"
+age = 19
+college = "KRMU"
+Branch = "CSE AI&ML"
+print ("Name :",name)
+print("Age :",age)
+print("College :",college)
+print("Branch:",Branch)
+print("================")
