eaf01289299864c4b034287b9458860fb682cb35
Tanu <tanusomani7004@gmail.com>
2026-08-06 22:49:04 +0530

    Create Armstrong-number-checker.py

---
+++ Armstrong-number-checker.py
@@ -0,0 +1,15 @@
+
+num = int(input("Enter a number: "))
+
+original = num
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + (digit ** 3)
+    num = num // 10
+
+if original == sum:
+    print(original, "is an Armstrong Number")
+else:
+    print(original, "is not an Armstrong Number")
